7543e4dee5c35c4051ea67d3e2d0a218c15b8ec9
kimbrow1 <you@example.com>
2024-05-03 20:48:25 -0500

    update

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 'use strict'
 
+// should declar _ instead of kodash
+const _ = require('_')
+
+const exampleArray = [1, [2, [3, [4,2,[9]],5]],5, [6]];
+
+const flatArray = _.flatten(exampleArray);
+
+ 
